9ee4a19318a602879b5fcbf2c958782b7fbde796
Tim Goodwin <trgoodwin@gmail.com>
2024-06-25 08:51:11 +0100

    implementing Playwright.Artifact to support download

---
+++ lib/playwright/artifact.ex
@@ -0,0 +1,25 @@
+defmodule Playwright.Artifact do
+  use Playwright.SDK.ChannelOwner
+
+  alias Playwright.Artifact
+  alias Playwright.SDK.ChannelOwner
+
+  @property :path
+
+  @impl ChannelOwner
+  def init(message, initializer) do
+    {:ok, %{message | path: initializer.absolute_path}}
+  end
+
+  def save_as(%Artifact{session: session, guid: guid}, path) do
+    Channel.post(session, {:guid, guid}, :save_as, %{path: path})
+  end
+
+  def delete(%Artifact{session: session, guid: guid}) do
+    Channel.post(session, {:guid, guid}, :delete)
+  end
+
+  def failure(%Artifact{session: session, guid: guid}) do
+    Channel.post(session, {:guid, guid}, :failure)
+  end
+end
