b6cb945a748ad39a305ef73ddcece0aa3cace433
Tim Goodwin <tim.goodwin@v7labs.com>
2026-02-17 18:04:15 +0000

    Fix Dialog accept/dismiss return values to match @spec
    Wrap Channel.post results in case statements to return :ok on success,
    matching the declared @spec and the established pattern in Frame.

--- lib/playwright/dialog.ex
+++ lib/playwright/dialog.ex
@@ -26,16 +26,22 @@ defmodule Playwright.Dialog do
   | ------------ | ---------- | ----------- |
   | `prompt_text`| `binary()` | Text to enter in a prompt dialog. Has no effect for other dialog types. |
   """
-  @spec accept(t(), binary()) :: :ok
+  @spec accept(t(), binary()) :: :ok | {:error, term()}
   def accept(%__MODULE__{session: session, guid: guid}, prompt_text \\ "") do
-    Channel.post(session, {:guid, guid}, :accept, %{prompt_text: prompt_text})
+    case Channel.post(session, {:guid, guid}, :accept, %{prompt_text: prompt_text}) do
+      {:ok, _} -> :ok
+      {:error, error} -> {:error, error}
+    end
   end
 
   @doc """
   Dismisses the dialog.
   """
-  @spec dismiss(t()) :: :ok
+  @spec dismiss(t()) :: :ok | {:error, term()}
   def dismiss(%__MODULE__{session: session, guid: guid}) do
-    Channel.post(session, {:guid, guid}, :dismiss, %{})
+    case Channel.post(session, {:guid, guid}, :dismiss, %{}) do
+      {:ok, _} -> :ok
+      {:error, error} -> {:error, error}
+    end
   end
 end
